90250ce7655034c898ca60c6a30e5e77c8470470
Aradhana Indra Daniswara <aradhanaindradev@gmail.com>
2023-08-09 16:08:39 +0700

    Add products, password and confirm password

--- pages/zod-regular/index.tsx
+++ pages/zod-regular/index.tsx
@@ -7,21 +7,58 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import wingsLogo from '@/public/wings.svg'
 import { zodResolver } from '@hookform/resolvers/zod';
+import { FormControl, FormField } from '@/components/ui/form'
+import { Checkbox } from '@/components/ui/checkbox'
 const inter = Inter({ subsets: ['latin'] })
 
-type Inputs = {
-	name: string
-	email: string
-	wantsMonthlyProduct: boolean
-	age: number
-}
+// type Inputs = {
+// 	name: string
+// 	email: string
+// 	wantsMonthlyProduct: boolean
+// 	age: number
+// }
+
+type Inputs = zod.infer<typeof InputsSchema>
 
 const InputsSchema = zod.object({
-	
-})
+	name: zod
+		.string({
+			required_error: "Name must be filled",
+			invalid_type_error: "Name must be a string!"
+		})
+		.min(3, "Name must be at least 3 characters"),
+	email: zod
+		.string({
+			required_error: "Email must be filled",
+			invalid_type_error: "Email must be a string!"
+		})
+		.email("Invalid email address!"),
+	age: zod.coerce
+		.number({
+			required_error: "Age must be filled",
+			invalid_type_error: "Age must be a number!"
+		})
+		.min(18, "You must be more than 18 years old to be a developer"),
+	password: zod
+		.string({
+			required_error: "Password must be filled",
+			invalid_type_error: "Password must be a string!"
+		})
+		.min(8, "Password must be at least 8 characters long"),
+	confirmPassword: zod
+		.string({
+			required_error: "Confirm Password must be filled",
+			invalid_type_error: "Confirm Password must be a string!"
+		})
+		.min(1, "Confirm password must be filled"),
+	wantsMonthlyProduct: zod
+		.boolean().default(false),
+}).refine(({ password, confirmPassword }) => (password === confirmPassword),{
+		message: "Password don't match"
+	})
 
 export default function ZodRegular() {
-	const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+	const { control, register, handleSubmit, formState: { errors } } = useForm<Inputs>({
 		resolver: zodResolver(InputsSchema)
 	})
 
@@ -32,27 +69,65 @@ export default function ZodRegular() {
 
 	return (
 		<main className={`flex min-h-screen justify-center p-12 ${inter.className}`}>
-			<div>
+			<div className="w-fit">
 				<Image src={wingsLogo} alt='WINGS' width={128} className="mx-auto" />
-				<h1 className='text-2xl my-8 font-semibold'>Developers Registration</h1>
-				<form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+				<h1 className='my-8 text-2xl font-semibold'>Developers Registration</h1>
+				<form onSubmit={handleSubmit(onSubmit)} className='space-y-4 w-fit'>
 
-					<div className="grid w-full max-w-sm items-center gap-2">
+					<div className="grid items-center w-full max-w-sm gap-2">
 						<Label htmlFor="name">Name</Label>
-						<Input type="text" id="name" {...register('name')} />
+						<Input type="text" id='name' {...register('name')} />
+						{errors.name?.message && <p className='mt-1 text-sm text-red-600'>{errors.name?.message}</p>}
 					</div>
 
-					<div className="grid w-full max-w-sm items-center gap-2">
+					<div className="grid items-center w-full max-w-sm gap-2">
 						<Label htmlFor="email">Email</Label>
 						<Input type="email" id="email" placeholder="example@mail.com" {...register('email')} />
+						{errors.email?.message && <p className='mt-1 text-sm text-red-600'>{errors.email?.message}</p>}
 					</div>
 
-					<div className="grid w-full max-w-sm items-center gap-2">
+					<div className="grid items-center w-full max-w-sm gap-2">
 						<Label htmlFor="age">Age</Label>
-						<Input type="number" id="age" {...register('age')} />
+						<Input type="number" id="age" {...register('age')} defaultValue={0} />
+						{errors.age?.message && <p className='mt-1 text-sm text-red-600'>{errors.age?.message}</p>}
 					</div>
 
-					<Button type='submit' className='w-full'>Register</Button>
+					<div className="grid items-center w-full max-w-sm gap-2">
+						<Label htmlFor="password">Password</Label>
+						<Input type="password" id="password" {...register('password')} />
+						{errors.password?.message && <p className='mt-1 text-sm text-red-600'>{errors.password?.message}</p>}
+					</div>
+
+					<div className="grid items-center w-full max-w-sm gap-2">
+						<Label htmlFor="confirmPassword">Confirm Password</Label>
+						<Input type="password" id="confirmPassword" {...register('confirmPassword')} />
+						{errors.confirmPassword?.message && <p className='mt-1 text-sm text-red-600'>{errors.confirmPassword?.message}</p>}
+					</div>
+
+					<div className="w-full">
+						<FormField
+							control={control}
+							name="wantsMonthlyProduct"
+							render={({ field }) => (
+								<div className='flex space-x-2'>
+									<Checkbox id="wantsMonthlyProduct"
+										checked={field.value}
+										onCheckedChange={field.onChange}
+									/>
+									<div className="leading-none">
+										<label
+											className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+										>
+											I want to receive WINGS&apos; Monthly Product
+										</label>
+										{errors.wantsMonthlyProduct?.message && <p className='w-full mt-1 text-sm text-red-600'>{errors.wantsMonthlyProduct?.message}</p>}
+									</div>
+								</div>
+							)}
+						/>
+					</div>
+
+					<Button type='submit' className='w-full max-w-sm'>Register</Button>
 				</form>
 			</div>
 		</main>
